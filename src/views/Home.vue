e2680e3b2769121dedf1d940a010823d6387899d
p-t-a-p-1 <kb240225+1@gmail.com>
2021-10-17 14:56:30 +0900

    [update] keyCountコンポーネント・テストコード追加

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,24 +1,27 @@
 <template>
   <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png" />
-    <HelloWorld msg="Welcome to Your Vue.js + TypeScript App" />
+    <!-- <img alt="Vue logo" src="../assets/logo.png" /> -->
+    <KeyCounter />
+    <!-- <HelloWorld msg="Welcome to Your Vue.js + TypeScript App" />
     <SampleText msg="aaaa" />
-    <Counter />
+    <Counter /> -->
   </div>
 </template>
 
 <script lang="ts">
 import { defineComponent } from "vue";
-import HelloWorld from "@/components/HelloWorld.vue"; // @ is an alias to /src
-import SampleText from "@/components/SampleText.vue";
-import Counter from "@/components/Counter.vue";
+// import HelloWorld from "@/components/HelloWorld.vue"; // @ is an alias to /src
+// import SampleText from "@/components/SampleText.vue";
+// import Counter from "@/components/Counter.vue";
+import KeyCounter from "@/components/KeyCounter.vue";
 
 export default defineComponent({
   name: "Home",
   components: {
-    HelloWorld,
-    SampleText,
-    Counter,
+    // HelloWorld,
+    // SampleText,
+    // Counter,
+    KeyCounter,
   },
 });
 </script>
